86e88ce0439231576f4db1880595f51f2ec44cdf
AyaNabil <ayanabil297@gmail.com>
2025-04-10 00:25:54 +0200

    Set Tailwind Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'deep-space': '#1C2526',
+          'neon-blue': '#00D4FF',
+          'cinematic-gold': '#FFD700',
+          'soft-white': '#F5F5F5',
+        },
+        fontFamily: {
+          orbitron: ['Orbitron', 'sans-serif'],
+          montserrat: ['Montserrat', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
